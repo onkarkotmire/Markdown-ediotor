cebec1ba0ec9c93b40acf0f7be7aed394a427f2c
unknown <onkarkotmire2017@gmail.com>
2023-03-14 12:55:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Markdown from './Components/Markdown';
+import Markdownreact from './Components/Markdownreact';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Markdown/> */}
+    <Markdownreact/>
+    </div>
+  );
+}
+
+export default App;
